ee1656625889a41c6271c8eeeef14eea694bb8cb
Dennis <randomspam1906@gmail.com>
2018-12-16 12:22:40 +0100

    Finished day 15 parts 1 and 2

--- Day15/Day15.py
+++ Day15/Day15.py
@@ -4,29 +4,39 @@ units = []
 
 class Unit(object):
 
-    def __init__(self, i, j, typ):
+    def __init__(self, i, j, typ, atk):
+        # base stats
         self.i = i
         self.j = j
         self.typ = typ
 
+        self.hp = 200
+        self.atk = atk
+
+        # keeping track of alive units
+        self.alive = True
+
         # distances reachable from a starting point (default is our own pos)
         self.dist_trace = []
-        self.init_dist_trace()
+        self.init_dist_trace((self.i, self.j))
+
+        # keeping track of occupied spaces (by others)
+        self.occupied = []
 
         # locations in range of an enemy
         self.in_range = []
 
-    def init_dist_trace(self):
-        # reset the distances (-1 everywhere except at our position)
+    def init_dist_trace(self, pos):
+        # reset the distances (-1 everywhere except at pos, which is where we start tracing)
         self.dist_trace = [[-1]*len(cave[i]) for i in xrange(len(cave))]
-        self.dist_trace[self.i][self.j] = 0
+        self.dist_trace[pos[0]][pos[1]] = 0
 
     def get_in_range(self):
         # get all positions in range of an enemy that are not occupied by a unit other than ourself
         self.in_range = []
         for unit in units:
             # has to be enemy
-            if unit.typ != self.typ:
+            if unit.typ != self.typ and unit.alive:
                 # directions
                 for di, dj in [(-1, 0), (1, 0),
                                (0, -1), (0, 1)]:
@@ -34,16 +44,24 @@ class Unit(object):
                     if 0 <= unit.i + di < len(cave) and 0 <= unit.j + dj < len(cave[unit.i + di]):
                         # mustn't be wall
                         if cave[unit.i + di][unit.j + dj] == "." and \
-                                (unit.i + di, unit.j + dj) not in [(u.i, u.j) if u != self else (-1, -1) for u in units]:
+                                (unit.i + di, unit.j + dj) not in self.occupied:
                             self.in_range.append((unit.i + di, unit.j + dj))
 
-    def trace(self, dist=1, start=None, reset=False):
-        if reset:
-            # reset the distance (for first call only)
-            self.init_dist_trace()
+    def get_occupied(self):
+        # finding all the occupied spaces
+        self.occupied = [(u.i, u.j) if u != self and u.alive else (-1, -1) for u in units]
+
+    def trace(self, start=None, reset=False, left_to_check=None):
         if start is None:
             # starting position
             start = (self.i, self.j)
+        if reset:
+            # reset the distance (for first call only)
+            self.init_dist_trace(start)
+        if left_to_check is None:
+            left_to_check = []
+
+        dist = self.dist_trace[start[0]][start[1]] + 1
 
         # check all directions
         for di, dj in [(-1, 0), (1, 0),
@@ -52,15 +70,20 @@ class Unit(object):
             if 0 <= start[0] + di < len(cave) and 0 <= start[1] + dj < len(cave[start[0] + di]):
                 # must be in cave and not occupied by anyone (ourself doesn't matter)
                 if cave[start[0] + di][start[1] + dj] == "." and \
-                        (start[0] + di, start[1] + dj) not in [(u.i, u.j) for u in units]:
+                        (start[0] + di, start[1] + dj) not in self.occupied:
                     # if distance is lower than for any path found earlier, or if it is the first path we found
                     # (in which case dist_trace will be -1, we change the value for the distance at this point
                     # we also start a new trace from this new starting point, with an increased distance from ourselves
-                    if dist <= self.dist_trace[start[0] + di][start[1] + dj] or \
+                    if dist < self.dist_trace[start[0] + di][start[1] + dj] or \
                             self.dist_trace[start[0] + di][start[1] + dj] == -1:
 
                         self.dist_trace[start[0] + di][start[1] + dj] = dist
-                        self.trace(dist=dist + 1, start=(start[0] + di, start[1] + dj))
+                        left_to_check.append((start[0] + di, start[1] + dj))
+
+        # to speed things up, we 'flow outward' from the starting point
+        if len(left_to_check) > 0:
+            new_start = left_to_check[0]
+            self.trace(start=new_start, left_to_check=left_to_check[1:])
 
     def move(self):
         # trace the distances
@@ -68,7 +91,7 @@ class Unit(object):
 
         # find all reachable points that are in range of an enemy and not occupied
         # in this case, the distance will no longer be -1
-        reachable = filter(lambda spot: self.dist_trace[spot[0]][spot[1]] >= 0, set(self.in_range))
+        reachable = filter(lambda spot: self.dist_trace[spot[0]][spot[1]] >= 0, self.in_range)
 
         if len(reachable) > 0:
             # if there are any reachable points, find the closest point
@@ -88,44 +111,112 @@ class Unit(object):
                            (0, 1), (1, 0)]:
                 if 0 <= self.i + di < len(cave) and 0 <= self.j + dj < len(cave[self.i + di]):
                     dist_this_dir = self.dist_trace[self.i + di][self.j + dj]
-                    if shortest_path == -1 or 0 < dist_this_dir < shortest_path:
-                        best_direction = (di, dj)
-                        shortest_path = dist_this_dir
+                    if 0 <= dist_this_dir:
+                        if shortest_path == -1 or dist_this_dir < shortest_path:
+                            best_direction = (di, dj)
+                            shortest_path = dist_this_dir
 
             # move unit
             self.i += best_direction[0]
             self.j += best_direction[1]
 
+    def get_hit(self, atk):
+        # get hit by enemy
+        self.hp -= atk
+        if self.hp <= 0:
+            self.alive = False
+
+    def attack(self):
+        # find enemies within our own range
+        units_in_range = filter(lambda i: units[i].alive and units[i].typ != self.typ and
+                                              abs(units[i].i - self.i) + abs(units[i].j - self.j) == 1, range(len(units)))
+
+        if len(units_in_range) > 0:
+            # choose the one with the lowest hp, and then in reading order
+            chosen = sorted(units_in_range, key=lambda i: (units[i].hp, units[i].i, units[i].j))[0]
+            units[chosen].get_hit(self.atk)
+
     def act(self):
+        # find all occupied spaces first (this messed me up at first)
+        self.get_occupied()
+        # then find all spots that are in range of enemies, and not occupied
         self.get_in_range()
-        if (self.i, self.j) in self.in_range:
-            pass
-        else:
+        # move if we can't attack, and try to attack afterwards
+        if not (self.i, self.j) in self.in_range:
             self.move()
+        self.attack()
 
 
-# read the cave
-with open("testinput.txt", "r") as f:
-    lines = f.readlines()
+# we will look for all attack powers
+elve_atk_pow = 3
+with open("input.txt", "r") as f:
+        lines = f.readlines()
+
+while True:
+    print elve_atk_pow
+
+    # we re-initialize everything
+    # the cave doesn't need to be re-initialized, but it is easier this way
+    no_elves = 0
+    cave = []
+    units = []
+
+    # read the cave
     for i in xrange(len(lines)):
         cave.append(lines[i].strip().replace("E", ".").replace("G", "."))
         for j in range(len(lines[i])):
-            if lines[i][j] in "EG":
-                units.append(Unit(i, j, lines[i][j]))
+            if lines[i][j] == "E":
+                units.append(Unit(i, j, "E", elve_atk_pow))
+                no_elves += 1
+            elif lines[i][j] == "G":
+                units.append(Unit(i, j, "G", 3))
 
-# loop used for testing
-for t in xrange(10):
-    units.sort(key=lambda u: (u.i, u.j))
-    for unit in units:
-        unit.act()
+    # simulation
+    t = 0
+    while True:
+        # take turns
+        units.sort(key=lambda u: (u.i, u.j))
+        for unit in units:
+            if unit.alive:
+                unit.act()
 
-    for i in xrange(len(cave)):
-        l = ""
-        for j in xrange(len(cave[i])):
-            for u in units:
-                if (u.i, u.j) == (i, j):
-                    l += u.typ
-                    break
+        # remove dead units
+        k = 0
+        while k < len(units):
+            if not units[k].alive:
+                del units[k]
             else:
-                l += cave[i][j]
-        print l
+                # remove # to see unit stats each round
+                # print t + 1, units[k].i, units[k].j, units[k].typ, units[k].hp
+                k += 1
+
+        # remove """ to see board every round
+        """
+        for i in xrange(len(cave)):
+            l = ""
+            for j in xrange(len(cave[i])):
+                for u in units:
+                    if u.alive and (u.i, u.j) == (i, j):
+                        l += u.typ
+                        break
+                else:
+                    l += cave[i][j]
+            print t + 1, l
+        """
+
+        # if there are only creatures of one type left, we stop the simulation
+        if len(set([u.typ for u in units])) == 1:
+            break
+
+        # otherwise, a full round has been completed, and we proceed
+        t += 1
+
+    # reporting part 1 and 2
+    if elve_atk_pow == 3:
+        print "PART 1", t, sum([u.hp for u in units]), t*sum([u.hp for u in units])
+    elif len(units) == no_elves and units[0].typ == "E":
+        print elve_atk_pow
+        print "PART 2", t, sum([u.hp for u in units]), t*sum([u.hp for u in units])
+        break
+
+    elve_atk_pow += 1
